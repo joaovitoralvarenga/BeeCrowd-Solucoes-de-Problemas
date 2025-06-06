1b34df0deace9a8208dfc188181d055958ee354f
joaovitoralvarenga <ggquennbr@gmail.com>
2025-06-05 22:54:05 -0300

    Valid Parentheses - 20 - Java - LeetCode - Sucess

--- LeetCode/Valid Parentheses - 20/Parentheses.java
+++ LeetCode/Valid Parentheses - 20/Parentheses.java
@@ -4,40 +4,33 @@ class Solution {
 
     boolean valida = true;
 
-    for(int i = 0;i<s.lenght();i++) {
+    for(int i = 0;i<s.length();i++) {
 
-      char c = s.charAt();
+        char c = s.charAt(i);
 
-      if(c == '(') {
-        expression.push(c);
-      }else if(c == ')' && !(expression.isEmpty())) {
-        expression.pop();
-      }else if(c == ')' || c =="}" || c == ']') {
-        valida = false;
-      }
+        if(c == '(' || c == '{' || c =='[') {
+            expression.push(c);                                   //Utiliza-se de uma pilha, a fim de garantir a inserção dos elementos que valida uma expressão
+        }else if(c == ')' || c == '}' || c == ']') {              //Se o outro elemento for um carectere que valida a mesma, o único elemento na pilha recebe pop
+            if(expression.isEmpty()) {                            //e a expressão é determinada como válida. A pila dessa forma fica vazia, garantindo maior eficiência. 
+                valida = false;
+            } else {
+                char top = expression.pop();
 
+                if((c == ')' && top != '(') ||
+                   (c == '}' && top != '{') ||                     //--> Seria melhor declararar um map para cada carectere a fim de evitar chegagem de condições grandes.
+                   (c == ']' && top != '[')) {
+                    valida = false;
+                   }
+            }
+        }
 
-    if(c == '{') {
-      expression.push(c);
-    }else if(c == '}' && !(expression.isEmpty())){
-      expression.pop();
-    }else if(c == '}' || c == ')' || c == ']') {
-      valida = false;
-    }
+        
 
-    if(c == '[') {
-      expression.push(c);
-    }else if(c == ']' && !(expression.isEmpty())){
-      expression.pop();
-    }else if(c == ']' || c == ')' || c == '}') {
-      valida = false;
-    }
-
-
-      
+     
     }
 
   
-    return valida;
+    return valida && expression.isEmpty();
   }
 }
+
